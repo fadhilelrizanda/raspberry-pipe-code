c429064ee86cd13adda73287c588684b3a0f53a2
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-07-11 22:36:35 +0700

    add motor code

--- motor.py
+++ motor.py
@@ -1,1 +1,41 @@
-print("motor")
+import RPi.GPIO as GPIO
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t','--time',type=int,required=True)
+parser.add_argument('-d','--direction',type=int,required=True)
+args= parser.parse_args()
+
+# Define pinout
+IN1 = 17
+IN2 = 27
+IN3 = 22
+IN4 = 5
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4,GPIO.OUT)
+
+def run_motor(time,direction):
+    print("Running Motor")
+    if direction == 1:
+        print("forward")
+        GPIO.output(IN1,GPIO.LOW)
+        GPIO.output(IN2,GPIO.HIGH)
+        GPIO.output(IN3,GPIO.LOW)
+        GPIO.output(IN4,GPIO.HIGH)
+    else:
+        print("backward")
+        GPIO.output(IN1,GPIO.LOW)
+        GPIO.output(IN2,GPIO.HIGH)
+        GPIO.output(IN3,GPIO.LOW)
+        GPIO.output(IN4,GPIO.HIGH)
+    time.sleep(time)
+    print(f"done {time} seconds")
+
+
+if __name__ == "__main__":
+    run_motor(args.time,args.direction)
