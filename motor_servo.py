b522f3311e02e3620a88c4d8135e449a24c9fe75
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-07-13 17:57:58 +0700

    fixing servo 4

--- motor_servo.py
+++ motor_servo.py
@@ -1,18 +1,15 @@
-import RPi.GPIO as GPIO
-import time
 import argparse
 from gpiozero import Servo
 
+# Argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--degree', type=int, required=True)
 parser.add_argument('-s', '--servo', type=int, required=True)
 args = parser.parse_args()
 
-# GPIO pin connected to the servo
-
+# GPIO pins connected to the servos
 servo1 = Servo(12)
 servo2 = Servo(13)
-
 
 def convert_to_range(value, min_value=0, max_value=180, new_min=-1, new_max=1):
     # Check if the value is within the expected range
@@ -28,14 +25,13 @@ def run_servo(servo_num, degree):
     pulsewidth_micros = convert_to_range(angle_degrees)
     # Create PWM instance
     if servo_num == 1:
-        print(pulsewidth_micros)
-        servo1.value(pulsewidth_micros)
-        print(f"Servo 1 running {pulsewidth_micros}")
+        servo1.value = pulsewidth_micros
+        print(f"Servo 1 running at {pulsewidth_micros}")
+    elif servo_num == 2:
+        servo2.value = pulsewidth_micros
+        print(f"Servo 2 running at {pulsewidth_micros}")
     else:
-        print(pulsewidth_micros)
-        servo2.value(pulsewidth_micros)
-        print(f"Servo 2 running {pulsewidth_micros}")
-
+        print("Invalid servo number. Please specify 1 or 2.")
 
 if __name__ == "__main__":
-    run_servo(args.servo,args.degree)
+    run_servo(args.servo, args.degree)
