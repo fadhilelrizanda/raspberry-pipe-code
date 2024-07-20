3c8d1606e64852d0e8d0a3b87e6c2358152a3477
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-07-20 17:46:39 +0700

    add servo

--- motor_servo.py
+++ motor_servo.py
@@ -1,14 +1,6 @@
 import argparse
-from gpiozero import Servo
-from gpiozero.pins.pigpio import PiGPIOFactory
-import subprocess
+import RPi.GPIO as GPIO
 import time
-
-# Start the pigpiod daemon
-subprocess.run(["sudo", "pigpiod"])
-
-# Allow some time for pigpiod to start
-time.sleep(1)
 
 # Argument parsing
 parser = argparse.ArgumentParser()
@@ -16,39 +8,32 @@ parser.add_argument('-d', '--degree', type=int, required=True)
 parser.add_argument('-s', '--servo', type=int, required=True)
 args = parser.parse_args()
 
-# Initialize pigpio pin factory
-factory = PiGPIOFactory()
-
 # GPIO pins connected to the servos
-servo1 = Servo(12, pin_factory=factory)
-servo2 = Servo(13, pin_factory=factory)
+Servo_pin1 = 12
+Servo_pin2 = 13
+GPIO.setup(Servo_pin1,GPIO.out)
+GPIO.setup(Servo_pin2,GPIO.out)
 
-def convert_to_range(value, min_value=0, max_value=180, new_min=-1, new_max=1):
-    # Check if the value is within the expected range
-    if value < min_value or value > max_value:
-        raise ValueError("Value out of range")
-    
-    # Perform the linear transformation
-    new_value = ((value - min_value) * (new_max - new_min) / (max_value - min_value)) + new_min
-    return new_value
+pwm = GPIO.PWM(Servo_pin1,50)
+pwm2 = GPIO.PWM(Servo_pin2,50)
 
-def run_servo(servo_num, degree):
-    angle_degrees = degree
-    pulsewidth_micros = convert_to_range(angle_degrees)
-    # Set servo value
-    if servo_num == 1:
-        servo1.value = pulsewidth_micros
-        print(f"Servo 1 running at {pulsewidth_micros}")
-    elif servo_num == 2:
-        servo2.value = pulsewidth_micros
-        print(f"Servo 2 running at {pulsewidth_micros}")
+pwm1.start(0)
+pwm2.start(0)
+
+def set_angle(angle,servo_num):
+    duty_cycle = (angle/18)+2
+    if servo_num==1:
+        GPIO.output(Servo_pin1,True)
+        pwm.ChangeDutyCycle(duty_cycle)
+        time.sleep(1)  # Wait for the servo to reach the angle
+
     else:
-        print("Invalid servo number. Please specify 1 or 2.")
+        GPIO.output(servo_pin, False)
+        pwm.ChangeDutyCycle(0)
+        time.sleep(1)
+    print(f"Run Servo {servo_num} {angle} {duty_cycle}")
 
 if __name__ == "__main__":
-    try:
-        run_servo(args.servo, args.degree)
-    finally:
-        # Stop the pigpiod daemon
-        subprocess.run(["sudo", "killall", "pigpiod"])
+    set_angle(args.degree, args.servo)
+
     
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,42 @@
+    import RPi.GPIO as GPIO
+    import time
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t','--time_sleep',type=int,required=True)
+    parser.add_argument('-d','--direction',type=int,required=True)
+    args= parser.parse_args()
+
+    # Define pinout
+    IN1 = 17
+    IN2 = 27
+    IN3 = 22
+    IN4 = 16
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(IN1, GPIO.OUT)
+    GPIO.setup(IN2, GPIO.OUT)
+    GPIO.setup(IN3, GPIO.OUT)
+    GPIO.setup(IN4,GPIO.OUT)
+
+    def run_motor(time_sleep,direction):
+        print("Running Motor")
+        if direction == 1:
+            print("forward")
+            GPIO.output(IN1,GPIO.LOW)
+            GPIO.output(IN2,GPIO.HIGH)
+            GPIO.output(IN3,GPIO.LOW)
+            GPIO.output(IN4,GPIO.HIGH)
+        else:
+            print("backward")
+            GPIO.output(IN1,GPIO.HIGH)
+            GPIO.output(IN2,GPIO.LOW)
+            GPIO.output(IN3,GPIO.HIGH)
+            GPIO.output(IN4,GPIO.LOW)
+        time.sleep(time_sleep)
+        print(f"done {time_sleep} second s")
+        GPIO.cleanup()
+
+
+    if __name__ == "__main__":
+        run_motor(args.time_sleep,args.direction)
