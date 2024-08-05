d45295259f4188051dc3b07fd4f924d87f8b2098
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-08-05 21:10:49 +0700

    test cam 2

--- stream.py
+++ stream.py
@@ -9,11 +9,7 @@ from libcamera import Transform
 # Function to capture frames and send over the network
 def stream_camera(connection):
     camera = Picamera2()
-    # Adjust the camera configuration to use the full sensor resolution
-    camera_config = camera.create_still_configuration(
-        main={"size": camera.sensor_resolution},  # Use the full sensor resolution
-        transform=Transform(hflip=1, vflip=1)
-    )
+    camera_config = camera.create_still_configuration(main={"size": (640, 480)}, transform=Transform(hflip=1, vflip=1))
     camera.configure(camera_config)
     camera.start()
 
