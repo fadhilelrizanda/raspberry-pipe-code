2e9b9417f7f7fe69f43bb1f2c9fe3088a18e5c00
fadhilelrizanda <fadhilelrizanda@gmail.com>
2024-08-06 09:33:53 +0700

    add servo

--- motor_servo.py
+++ motor_servo.py
@@ -2,39 +2,42 @@ import argparse
 import RPi.GPIO as GPIO
 import time
 
-
 # Argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--degree', type=int, required=True, help="Desired angle for the servo (0-180)")
 parser.add_argument('-s', '--servo', type=int, required=True, choices=[1, 2], help="Servo number (1 or 2)")
 args = parser.parse_args()
 
-# GPIO pins connected to the servos pin 12 and 13
+# GPIO pins connected to the servos
 Servo_pin1 = 12
 Servo_pin2 = 13
 GPIO.setmode(GPIO.BCM)  # Use board pin numbering
 GPIO.setup(Servo_pin1, GPIO.OUT)
 GPIO.setup(Servo_pin2, GPIO.OUT)
 
-pwm = GPIO.PWM(Servo_pin1, 50)
+pwm1 = GPIO.PWM(Servo_pin1, 50)
 pwm2 = GPIO.PWM(Servo_pin2, 50)
 
-pwm.start(0)
+pwm1.start(0)
 pwm2.start(0)
 
-def set_angle(angle, servo_num):
+def set_angle(angle, servo_num, step=1, delay=0.02):
     if servo_num == 1:
-        print("Running Servo 1")
-        pwm.ChangeDutyCycle(2 + (angle / 18))  # Calculate duty cycle for Servo 1
-        time.sleep(1)  # Wait for the servo to reach the angle
-        # pwm.ChangeDutyCycle(0)  # Stop sending PWM signal
+        pwm = pwm1
     elif servo_num == 2:
-        print("Running Servo 2")
-        pwm2.ChangeDutyCycle(2 + (angle / 18))  # Calculate duty cycle for Servo 2
-        time.sleep(1)  # Wait for the servo to reach the angle
-        # pwm2.ChangeDutyCycle(0)  # Stop sending PWM signal
+        pwm = pwm2
     else:
         print("Invalid servo number. Please use 1 or 2.")
+        return
+    
+    current_angle = 0
+    for i in range(0, angle + 1, step):
+        duty_cycle = 2 + (i / 18)
+        pwm.ChangeDutyCycle(duty_cycle)
+        time.sleep(delay)
+
+    time.sleep(1)
+    pwm.ChangeDutyCycle(0)
 
 if __name__ == "__main__":
     set_angle(args.degree, args.servo)
